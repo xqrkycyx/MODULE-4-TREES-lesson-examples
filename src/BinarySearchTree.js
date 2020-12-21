bc5fb7492757e41d28361389d0c4cb70702c56a1
kchia <kchia87@gmail.com>
2020-12-20 15:52:31 -1000

    add complete code for bfs

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,5 @@
+import Queue from "./Queue";
+
 class BinarySearchTree {
   constructor(key = null, value = null, parent = null) {
     this.key = key;
@@ -94,6 +96,25 @@ class BinarySearchTree {
     return values;
   }
 
+  bfs(tree, values = []) {
+    const queue = new Queue();
+    const node = tree.root;
+    queue.enqueue(node);
+    while (queue.length) {
+      const node = queue.dequeue();
+      values.push(node.value);
+
+      if (node.left) {
+        queue.enqueue(node.left);
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right);
+      }
+    }
+
+    return values;
+  }
   _replaceWith(node) {
     if (this.parent) {
       if (this == this.parent.left) {
